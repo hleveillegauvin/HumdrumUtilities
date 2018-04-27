f51bea47e072de135bbf8678d89890daf2b97049
Hubert Léveillé Gauvin <31942487+hleveillegauvin@users.noreply.github.com>
2018-04-27 11:20:14 -0400

    Update humdrumutils.sh

--- humdrumutils.sh
+++ humdrumutils.sh
@@ -66,7 +66,7 @@ humsed -E '
 /^[^=]/ s/^(G|d)\[/13\[/g
 /^[^=]/ s/^(A-|G#|e-|d#|a-|g#|aa-|gg#)\[/23\[/g
 /^[^=]/ s/^(F#|G-|c#|d-)_/123_/g
-# Middle tie
+# End tie
 /^[^=]/ s/^(c|g|cc|ee|gg|ccc|eee|ggg)\]/0\]/g
 /^[^=]/ s/^(B-|A#|f|b-|a#|dd|ff|bb-|aa#|ddd|fff)\]/1\]/g
 /^[^=]/ s/^(B|f#|g-|b|dd#|ee-|ff#|gg-|bb|ddd#|eee-|fff#|ggg-)\]/2\]/g
@@ -74,7 +74,7 @@ humsed -E '
 /^[^=]/ s/^(G|d)\]/13\]/g
 /^[^=]/ s/^(A-|G#|e-|d#|a-|g#|aa-|gg#)\]/23\]/g
 /^[^=]/ s/^(F#|G-|c#|d-)\]/123\]/g
-# End tie
+# Middle tie
 /^[^=]/ s/^(c|g|cc|ee|gg|ccc|eee|ggg)_/0_/g
 /^[^=]/ s/^(B-|A#|f|b-|a#|dd|ff|bb-|aa#|ddd|fff)_/1_/g
 /^[^=]/ s/^(B|f#|g-|b|dd#|ee-|ff#|gg-|bb|ddd#|eee-|fff#|ggg-)_/2_/g
